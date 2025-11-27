95c02495a52f57f1c93d4b35efc5e8d8e26d4e69
GeralddTheWay <abiarka060109@gmail.com>
2025-11-27 21:49:21 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll effect
+
+
+// Popup message on form submit
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Terima kasih! Pesan Anda telah terkirim.');
+});
